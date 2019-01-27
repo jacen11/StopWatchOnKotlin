2a2670b456b8eca948aef08a898d60fecc54b4c3
jacen11 <jacen19931@gmail.com>
2019-01-27 21:12:16 +0300

    пауза в любом случае

--- app/src/main/java/com/example/stopwatchonkotlin/MainActivity.kt
+++ app/src/main/java/com/example/stopwatchonkotlin/MainActivity.kt
@@ -3,7 +3,6 @@ package com.example.stopwatchonkotlin
 import android.annotation.SuppressLint
 import android.os.Bundle
 import android.os.Handler
-import android.os.PersistableBundle
 import android.support.v7.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 import java.util.*
@@ -13,15 +12,16 @@ class MainActivity : AppCompatActivity() {
 
     private var seconds: Int = 0
     private var running: Boolean = false
-
+    private var wasRunning = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        if(savedInstanceState!=null){
+        if (savedInstanceState != null) {
             seconds = savedInstanceState.getInt("seconds")
             running = savedInstanceState.getBoolean("running")
+            wasRunning = savedInstanceState.getBoolean("wasRunning")
         }
 
         runTimer()
@@ -40,21 +40,35 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    override fun onResume() {
+        super.onResume()
+        if (wasRunning) {
+            running = true
+        }
+    }
+
+    override fun onPause() {
+        super.onPause()
+        wasRunning = running
+        running = false
+    }
+
     @SuppressLint("MissingSuperCall")
     override fun onSaveInstanceState(savedInstanceState: Bundle?) {
-        savedInstanceState?.putInt("seconds",seconds)
+        savedInstanceState?.putInt("seconds", seconds)
         savedInstanceState?.putBoolean("running", running)
+        savedInstanceState?.putBoolean("wasRunning", wasRunning)
     }
 
     private fun runTimer() {
         val handler: Handler = Handler()
         handler.post(object : Runnable {
             override fun run() {
-                var hours = seconds / 3600
-                var minutes = (seconds % 3600) / 60
-                var secs = seconds % 60
+                val hours = seconds / 3600
+                val minutes = (seconds % 3600) / 60
+                val secs = seconds % 60
 
-                var time = String.format(Locale.getDefault(), "%d:%02d:%02d", hours, minutes, secs)
+                val time = String.format(Locale.getDefault(), "%d:%02d:%02d", hours, minutes, secs)
                 time_view.text = time
                 if (running) {
                     seconds++
